c13ccdbe92ca3335180e6121f7bf8b392fb4bfdc
Charlles Abreu <abreu@eq.ufrj.br>
2019-02-28 17:54:48 -0500

    Inclusion of PINY test with dt=1fs

--- PINY/job_rusalka.sh
+++ PINY/job_rusalka.sh
@@ -2,9 +2,9 @@
 #SBATCH -J sinr           # Job name
 #SBATCH -o job.%j.out     # Name of stdout output file
 #SBATCH -N 1              # Total number of nodes requested
-#SBATCH -n 7              # Total number of mpi tasks requested
+#SBATCH -n 8              # Total number of mpi tasks requested
 #SBATCH --mem=10GB        # To specify required memory per node
-#SBATCH -t 120:00:00      # Run time (hh:mm:ss) - 24 hours
+#SBATCH -t 180:00:00      # Run time (hh:mm:ss) - 24 hours
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ca2356@nyu.edu
 
@@ -13,4 +13,4 @@ module load gnu8/8.2.0
 module load openmpi3/3.1.2
 module load fftw/3.3.8
 
-parallel ./run_sinr.sh ::: 03 06 09 15 30 45 90
+parallel ./run_sinr.sh ::: 01 03 06 09 15 30 45 90
