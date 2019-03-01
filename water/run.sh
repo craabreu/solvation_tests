2838e3eb4791344ad9aa22d7ce9c92f9ee9ebffe
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-01 18:39:22 -0500

    Bug fix in water tests

--- water/run.sh
+++ water/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-case+=("1")
+case+=("01")
 nsteps+=("3600000")
 device+=("0")
 
-case+=("3")
+case+=("03")
 nsteps+=("1200000")
 device+=("1")
 
-case+=("6")
+case+=("06")
 nsteps+=("600000")
 device+=("2")
 
-case+=("9")
+case+=("09")
 nsteps+=("400000")
 device+=("3")
 
@@ -32,7 +32,5 @@ case+=("90")
 nsteps+=("40000")
 device+=("3")
 
-mkdir -p sinr-${case[$1]}fs
 cd sinr-${case[$1]}fs
-cp ../water.* .
 python simulate.py --device ${device[$1]} --timestep ${case[$1]} --nsteps ${nsteps[$1]}
