0bad23922736a2b1ec873f51b80fc540d2867542
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-02 16:35:32 -0500

    Bug fix

--- water/piny/run_sinr.sh
+++ water/piny/run_sinr.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-cd sinr-${1}fs
+name=sinr-${1}fs
+cd $name
 
 # Clean directory and run simulation:
 ./tidy
-./run > sinr-${1}fs.output
+./run > $name.output
 
 # Convert trajectory file to xyz format:
-python ../conf-to-traj.py --out $1.xyz --decimal 8 water.confp
+python ../conf-to-traj.py --out $name.xyz --decimal 8 water.confp
 
 # Compute radial distribution functions:
 mkdir -p rdf
 cd rdf
-../../../../travis/exe/travis -i ../../../travis_rdf.inp -p ../$1.xyz
+../../../../travis/exe/travis -i ../../../travis_rdf.inp -p ../$name.xyz
 gOO="rdf_H2O_#2_H2O_[Or_Oo].csv"
 gOH="rdf_H2O_#2_H2O_[Or_Ho].csv"
 gHH="rdf_H2O_#2_H2O_[Hr_Ho].csv"
@@ -19,5 +20,5 @@ gIntra="rdf_H2O_#2_H2O_[Or_Ho].csv"
 paste -d"," <(cut -d";" -f1 $gOO | sed 's/# //g')  \
             <(cut -d";" -f2 $gOO | sed 's/g(r)/g(O-O)/g') \
             <(cut -d";" -f2 $gOH | sed 's/g(r)/g(O-H)/g') \
-            <(cut -d";" -f2 $gHH | sed 's/g(r)/g(H-H)/g') > ../${1}_rdf.csv
-sed 's/;/,/g' $gIntra > ../${1}_bond.csv
+            <(cut -d";" -f2 $gHH | sed 's/g(r)/g(H-H)/g') > ../${name}_rdf.csv
+sed 's/;/,/g' $gIntra > ../${name}_bond.csv
