f68c319ccc5f1064db4983b21b4ba3ad7ab47072
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-02 18:50:36 -0500

    Update of bond distribution results

--- water/compute_rdf.sh
+++ water/compute_rdf.sh
@@ -11,7 +11,7 @@ $root/travis/exe/travis -i $root/water/travis_rdf.inp -p ../$name.xyz > ../${nam
 gOO="rdf_H2O_#2_H2O_[Or_Oo].csv"
 gOH="rdf_H2O_#2_H2O_[Or_Ho].csv"
 gHH="rdf_H2O_#2_H2O_[Hr_Ho].csv"
-gIntra="rdf_H2O_#2_H2O_[Or_Hr].csv"
+gIntra="rdf_H2O_#2_[Or_Hr].csv"
 paste -d"," <(cut -d";" -f1 $gOO | sed 's/# //g')  \
             <(cut -d";" -f2 $gOO | sed 's/g(r)/g(O-O)/g') \
             <(cut -d";" -f2 $gOH | sed 's/g(r)/g(O-H)/g') \
