eec01c272a9de810cb49a0f170bd7cb4aca388e4
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-02 17:38:56 -0500

    Change in analysis script location

---
+++ water/analyze_sinr.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./compute_rdf.sh $@
--- water/piny/run_sinr.sh
+++ water/piny/run_sinr.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 name=sinr-${1}fs
-root=../../../..
 cd $name
 
 # Clean directory and run simulation:
@@ -9,17 +8,3 @@ cd $name
 
 # Convert trajectory file to xyz format:
 python ../conf-to-traj.py --out $name.xyz --decimal 8 water.confp
-
-# Compute radial distribution functions:
-mkdir -p rdf
-cd rdf
-$root/travis/exe/travis -i $root/water/travis_rdf.inp -p ../$name.xyz > ../${name}_rdf.output
-gOO="rdf_H2O_#2_H2O_[Or_Oo].csv"
-gOH="rdf_H2O_#2_H2O_[Or_Ho].csv"
-gHH="rdf_H2O_#2_H2O_[Hr_Ho].csv"
-gIntra="rdf_H2O_#2_H2O_[Or_Ho].csv"
-paste -d"," <(cut -d";" -f1 $gOO | sed 's/# //g')  \
-            <(cut -d";" -f2 $gOO | sed 's/g(r)/g(O-O)/g') \
-            <(cut -d";" -f2 $gOH | sed 's/g(r)/g(O-H)/g') \
-            <(cut -d";" -f2 $gHH | sed 's/g(r)/g(H-H)/g') > ../${name}_rdf.csv
-sed 's/;/,/g' $gIntra > ../${name}_bond.csv
