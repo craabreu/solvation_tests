60dc2157a61f31b323ae9871642878bc689b09ab
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-02 19:33:42 -0500

    Correction of bond length distribution results

--- water/compute_bond.sh
+++ water/compute_bond.sh
@@ -8,5 +8,5 @@ mkdir -p rdf
 cd rdf
 root=../../../..
 $root/travis/exe/travis -i $root/water/travis_bond.inp -p ../$name.xyz > ../${name}_bond.output
-gIntra="rdf_H2O_#2_H2O_[Or_Ho].csv"
+gIntra="rdf_H2O_#2_[Or_Hr].csv"
 sed 's/;/,/g' $gIntra > $root/water/$dir/results/${name}_bond.csv
