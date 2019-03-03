9a914ac213104dfd4c62251f9a7b2ed46a11cbc9
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-02 21:48:06 -0500

    Plot update

--- water/plots.py
+++ water/plots.py
@@ -3,58 +3,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import figstyle
+import os
 
-cases = ['piny']
-timesteps = [6, 9, 15, 30, 45, 90]
+tools = ['piny', 'openmm']
+timesteps = [1, 3, 6, 9, 15, 30, 45, 90]
+
+def data(type):
+    repo = {tool:[] for tool in tools}
+    for dt, tool in itertools.product(timesteps, tools):
+        file = f'{tool}/results/sinr-{dt:02d}fs_{type}.csv'
+        if os.path.isfile(file):
+            repo[tool].append(pd.read_csv(file, skipinitialspace=True))
+    return repo
 
 # Intermolecular radial distribution functions:
-rdf = {case:[] for case in cases}
-for dt, case in itertools.product(timesteps, cases):
-    rdf[case].append(pd.read_csv(f'{case}/results/sinr-{dt:02d}fs_rdf.csv', skipinitialspace=True))
-
-for case in cases:
+rdf = data('rdf')
+for tool in tools:
     fig, ax = plt.subplots(3, 1, figsize=(3.37,5.6), sharex=True)
-    fig.suptitle(f'Radial distribution functions ({case})')
+    fig.suptitle(f'Radial distribution functions ({tool})')
     fig.subplots_adjust(hspace=0.1)
     ax[-1].set_xlabel('Distance (\\AA)')
-    for dt, gr in zip(timesteps, rdf[case]):
+    for dt, gr in zip(timesteps, rdf[tool]):
         distance = gr['Distance [pm]']/100
         for i, pair in enumerate(['O-O', 'O-H', 'H-H']):
             ax[i].plot(distance, gr[f'g({pair})'], label=f'{dt} fs')
             ax[i].set_ylabel(f'g({pair})')
     ax[2].legend(loc='lower right', ncol=2)
-    fig.savefig(f'{case}_rdf.png')
+    fig.savefig(f'{tool}_rdf.png')
 
 # Bond length distributions:
-bond = {case:[] for case in cases}
-for dt, case in itertools.product(timesteps, cases):
-    bond[case].append(pd.read_csv(f'{case}/results/sinr-{dt:02d}fs_bond.csv', skipinitialspace=True))
-
-for case in cases:
+bond = data('bond')
+for tool in tools:
     fig, ax = plt.subplots(1, 1, figsize=(3.37,2.3), sharex=True)
-    fig.suptitle(f'Bond length distributions ({case})')
+    fig.suptitle(f'Bond length distributions ({tool})')
     ax.set_xlabel('Distance (\\AA)')
-    for dt, gr in zip(timesteps, bond[case]):
+    for dt, gr in zip(timesteps, bond[tool]):
         distance = gr['# Distance [pm]']/100
         ax.plot(distance, gr['g(r)'], label=f'{dt} fs')
         ax.set_ylabel(f'frequency')
     ax.legend(loc='upper left', ncol=1)
-    fig.savefig(f'{case}_bond.png')
+    fig.savefig(f'{tool}_bond.png')
 
 # Angle distributions:
-angle = {case:[] for case in cases}
-for dt, case in itertools.product(timesteps, cases):
-    angle[case].append(pd.read_csv(f'{case}/results/sinr-{dt:02d}fs_angle.csv', skipinitialspace=True))
-
-for case in cases:
+angle = data('angle')
+for tool in tools:
     fig, ax = plt.subplots(1, 1, figsize=(3.37,2.3), sharex=True)
-    fig.suptitle(f'Angle distributions ({case})')
+    fig.suptitle(f'Angle distributions ({tool})')
     ax.set_xlabel('Angle (\\textdegree)')
-    for dt, gr in zip(timesteps, angle[case]):
+    for dt, gr in zip(timesteps, angle[tool]):
         distance = gr['# Angle (degree)']
         ax.plot(distance, gr['Occurrence'], label=f'{dt} fs')
         ax.set_ylabel(f'frequency')
     ax.legend(loc='upper left', ncol=1)
-    fig.savefig(f'{case}_angle.png')
+    fig.savefig(f'{tool}_angle.png')
 
 plt.show()
