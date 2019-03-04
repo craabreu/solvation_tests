23fe862b7d16f8f1643a481d628c036bb502a356
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-04 16:40:11 -0500

    Combined distribution results

--- water/analyze_sinr.sh
+++ water/analyze_sinr.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Usage: ./analyze_sinr.sh [piny/openmm] [delta-t]
-#./compute_rdf.sh $@
+./compute_rdf.sh $@
 #./compute_bond.sh $@
 #./compute_angle.sh $@
+./compute_bond_and_angle.sh $@
 ./compute_sdf.sh $@
