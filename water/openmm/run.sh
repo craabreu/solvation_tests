f597bb2d357153a325d5e3768cdf7d006a74565a
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-05 17:31:29 -0500

    OpenMM properties averaged

--- water/openmm/run.sh
+++ water/openmm/run.sh
@@ -48,5 +48,14 @@ secdev+=("1")
 name=sinr-${case[$1]}fs
 cd $name
 #python simulate.py --device ${device[$1]} --secdev ${secdev[$1]} --timestep ${case[$1]} --nsteps ${nsteps[$1]}
-babel -ipdb $name.pdb -oxyz $name.xyz
-sed -i -e 's/Ow/O/g' -e 's/Hw/H/g' $name.xyz
+#babel -ipdb $name.pdb -oxyz $name.xyz
+#sed -i -e 's/Ow/O/g' -e 's/Hw/H/g' $name.xyz
+
+properties='Temp PotEng Press MolPress Virial NBVirial MolVirial'
+dt=$(echo ${case[$1]} | sed 's/p/./')
+values=$(postlammps -d comma -mm -c 83 -nt ineff $properties < ${name}.csv | \
+         cut -d',' -f2,3 | \
+         tr '\n' ',' | \
+         sed 's/,$//')
+line=$(echo "$dt,$values")
+eval "sed -i 's/^$dt.*/$line/' ../results/properties.csv"
