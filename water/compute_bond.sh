a8e836fec532d8028faa41f6b87a8a5d3f009a1d
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-09 18:57:36 -0500

    Some corrections

--- water/compute_bond.sh
+++ water/compute_bond.sh
@@ -7,9 +7,9 @@ cd $dir/$name
 mkdir -p bdf
 cd bdf
 root=../../../..
-$root/travis/exe/travis -i $root/water/travis_bond.inp -p ../$name.xyz > ${name}_bond.output
-file="rdf_H2O_#2_[Or_Hr].csv"
-sed 's/;/,/g' $file > $root/water/$dir/results/${name}_bond.csv
+#$root/travis/exe/travis -i $root/water/travis_bond.inp -p ../$name.xyz > ${name}_bond.output
+#file="rdf_H2O_#2_[Or_Hr].csv"
+#sed 's/;/,/g' $file > $root/water/$dir/results/${name}_bond.csv
 
 values=$(grep "Mean value" ${name}_bond.output | sed -e 's/:/,/g' -e 's/pm/,/g' | cut -d"," -f2,4 | sed 's/ //g')
 dt=$(echo $2 | sed 's/p/./')
